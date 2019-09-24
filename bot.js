2e57a2723b05831815d56091dd2f0d056eccb38c
ndhuy2308 <31300525+ndhuy2308@users.noreply.github.com>
2019-09-24 21:20:46 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,6 +5,22 @@ const client = new Discord.Client();
 client.once('ready', () => {
 	console.log('Ready!');
 })
+
+client.on("guildMemberAdd", (member) => { // Check out previous chapter for information about this event
+let guild = member.guild; 
+let memberTag = member.user.tag; 
+if(guild.systemChannel){
+	guild.systemChannel.send(new Discord.RichEmbed() // Creating instance of Discord.RichEmbed
+	.setTitle("Một con gà mới đã vào Discord Newlight: ") // Calling method setTitle on constructor. 
+	.setDescription(memberTag + " #newlight") // Setting embed description
+	.setThumbnail(member.user.displayAvatarURL) // The image on the top right; method requires an url, not a path to file!
+	.addField("Số lượng thành viên hiện tại: ", member.guild.memberCount) // Adds a field; First parameter is the title and the second is the value.
+	.setTimestamp() // Sets a timestamp at the end of the embed
+	);
+}
+});
+
+
 client.on('message', message => {
   //console.log(message.content);
 	if(message.content.startsWith(`${prefix}dhuy`)) {
