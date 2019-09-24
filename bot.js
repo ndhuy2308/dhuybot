b8d69623829c6f9caf0c0ce34ba6e5eff81e7914
ndhuy2308 <31300525+ndhuy2308@users.noreply.github.com>
2019-09-24 21:28:42 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -32,5 +32,23 @@ client.on('message', message => {
 	if(message.content.startsWith(`${prefix}tkb`)) {
 		message.channel.send("BUỔI SÁNG:\nThứ 2: Văn - Văn - Toán - Toán\nThứ 3: Lý - Lý - Hóa - Sinh - Sinh\nThứ 4: Anh - Anh - Toán - Lý - GDCD\nThứ 5: Văn - Văn - Anh - Sử - Địa\nThứ 6: Hóa - Hóa - Toán - Toán - Sinh hoạt\n------------------------\nBUỔI CHIỀU:\nThứ 2 (Tiết 2345): Thể dục - Thể dục - Lý - Lý\nThứ 4 (Tiết 234): KTCN - KTCN - Quốc phòng \nThứ 6 (Tiết 2345): Toán - Toán - Tin - Tin")	
 	}
+	if(message.content.startsWith(`${prefix}tkb`)) {
+		message.channel.send("BUỔI SÁNG:\nThứ 2: Văn - Văn - Toán - Toán\nThứ 3: Lý - Lý - Hóa - Sinh - Sinh\nThứ 4: Anh - Anh - Toán - Lý - GDCD\nThứ 5: Văn - Văn - Anh - Sử - Địa\nThứ 6: Hóa - Hóa - Toán - Toán - Sinh hoạt\n------------------------\nBUỔI CHIỀU:\nThứ 2 (Tiết 2345): Thể dục - Thể dục - Lý - Lý\nThứ 4 (Tiết 234): KTCN - KTCN - Quốc phòng \nThứ 6 (Tiết 2345): Toán - Toán - Tin - Tin")	
+	}
+	if(message.content.startsWith(`${prefix}info`)) {
+		client.on("guildMemberAdd", (member) => { // Check out previous chapter for information about this event
+let guild = member.guild; 
+let memberTag = member.user.tag; 
+if(guild.systemChannel){
+	guild.systemChannel.send(new Discord.RichEmbed() // Creating instance of Discord.RichEmbed
+	.setTitle("A new user joined") // Calling method setTitle on constructor. 
+	.setDescription(memberTag + " has joined the guild") // Setting embed description
+	.setThumbnail(member.user.displayAvatarURL) // The image on the top right; method requires an url, not a path to file!
+	.addField("Members now", member.guild.memberCount) // Adds a field; First parameter is the title and the second is the value.
+	.setTimestamp() // Sets a timestamp at the end of the embed
+	);
+}
+});
+	}
 })
 client.login(process.env.BOT_TOKEN);
