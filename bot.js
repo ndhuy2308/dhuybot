a463143492f806ab545c10a72954f217da6f73b9
ndhuy2308 <31300525+ndhuy2308@users.noreply.github.com>
2019-09-24 21:08:53 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,6 +10,9 @@ client.on('message', message => {
 	if(message.content.startsWith(`${prefix}dhuy`)) {
 		message.channel.send("Tui xin loi")
 		}
+	if(message.content.startsWith(`${prefix}sin`)) {
+		message.channel.send("Khungg")
+		}
 	if(message.content.startsWith(`${prefix}tkb`)) {
 		message.channel.send("BUỔI SÁNG:\nThứ 2: Văn - Văn - Toán - Toán\nThứ 3: Lý - Lý - Hóa - Sinh - Sinh\nThứ 4: Anh - Anh - Toán - Lý - GDCD\nThứ 5: Văn - Văn - Anh - Sử - Địa\nThứ 6: Hóa - Hóa - Toán - Toán - Sinh hoạt\n------------------------\nBUỔI CHIỀU:\nThứ 2 (Tiết 2345): Thể dục - Thể dục - Lý - Lý\nThứ 4 (Tiết 234): KTCN - KTCN - Quốc phòng \nThứ 6 (Tiết 2345): Toán - Toán - Tin - Tin")	
 	}
