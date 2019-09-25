3de96b67bd4ad06a063580d2a74547107e746224
ndhuy2308 <31300525+ndhuy2308@users.noreply.github.com>
2019-09-25 17:51:33 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const prefix = "#";
+const superagent = require("superagent");
 const client = new Discord.Client();
-
 client.once('ready', () => {
 	console.log('Ready!');
 })
