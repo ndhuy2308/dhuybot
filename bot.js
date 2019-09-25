010d843f024a0b8cf482555f2d43776b22e1e138
ndhuy2308 <31300525+ndhuy2308@users.noreply.github.com>
2019-09-25 16:10:58 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -30,7 +30,7 @@ client.on('message', (message, member) => {
 	if(message.content.startsWith(`${prefix}sin`)) {
 		message.channel.send("Khungg")
 		}
-	if(message.content.startsWith(`${prefix}tkb`) && uidstring == "493299951590440961") {
+	if(message.content.startsWith(`${prefix}tkb`) && uidstring == "493299951590440961" || uidstring == "479877128368685056") {
 		var embed = new Discord.RichEmbed()
 		.setColor('#0099ff')
   		.setAuthor("DHuy BOT - Thời khóa biểu", message.author.avatarURL)
