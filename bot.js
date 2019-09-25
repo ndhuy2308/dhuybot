206a5fbdf6a7feb739a692a57cc7993c92734c09
ndhuy2308 <31300525+ndhuy2308@users.noreply.github.com>
2019-09-25 21:04:36 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -23,9 +23,6 @@ client.on('message', (message, member) => {
   //console.log(message.content);
 	var uid = message.author.id;
 	var uidstring = "" + uid;
-	if(message.content.startsWith(`${prefix}delete`) && uidstring == "493299951590440961" || message.content.startsWith(`${prefix}delete`) && uidstring == "479877128368685056") {
-		message.channel.send("pls clean 100")
-	}
 	if(message.content.startsWith(`${prefix}dhuy`) && uidstring == "493299951590440961") {
 		message.channel.send("Tui xin loi")
 		}
