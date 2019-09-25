de9fa9eb6415ae89d2c0cfecfeb28ab5f2a0fdc1
ndhuy2308 <31300525+ndhuy2308@users.noreply.github.com>
2019-09-25 15:10:32 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -35,7 +35,8 @@ client.on('message', (message, member) => {
 		var embed = new Discord.RichEmbed()
   		.setTitle("Thời khóa biểu")
   		.setAuthor("DHuy BOT", message.author.avatarURL)
-  		.setDescription(message.author.username);
+		.addfield("Thứ 2","Một 2 3 4")
+  		.setDescription("#tkb");
   		message.channel.send(embed);
 	}
 })
