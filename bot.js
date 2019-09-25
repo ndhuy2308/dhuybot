b65d66f967b600108a363f62cea5a6a59c9c7216
ndhuy2308 <31300525+ndhuy2308@users.noreply.github.com>
2019-09-25 15:27:03 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -45,7 +45,7 @@ client.on('message', (message, member) => {
 		.addField("Thứ 2 (Tiết 2345):","Thể dục - Thể dục - Lý - Lý")
 		.addField("Thứ 4 (Tiết 234):","KTCN - KTCN - Quốc phòng ")
 		.addField("Thứ 6 (Tiết 2345):","Toán - Toán - Tin - Tin")
-		.setFooter('ID', message.author.id)
+		.setFooter("ID", message.author.id)
 		;
   		message.channel.send(embed);
 	}
