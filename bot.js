3a9b16f3a3a160ffa440f93fe4f9bd5c2d61fc31
ndhuy2308 <31300525+ndhuy2308@users.noreply.github.com>
2019-09-25 14:32:16 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -32,7 +32,8 @@ client.on('message', message => {
 		message.channel.send("BUỔI SÁNG:\nThứ 2: Văn - Văn - Toán - Toán\nThứ 3: Lý - Lý - Hóa - Sinh - Sinh\nThứ 4: Anh - Anh - Toán - Lý - GDCD\nThứ 5: Văn - Văn - Anh - Sử - Địa\nThứ 6: Hóa - Hóa - Toán - Toán - Sinh hoạt\n------------------------\nBUỔI CHIỀU:\nThứ 2 (Tiết 2345): Thể dục - Thể dục - Lý - Lý\nThứ 4 (Tiết 234): KTCN - KTCN - Quốc phòng \nThứ 6 (Tiết 2345): Toán - Toán - Tin - Tin")	
 	}
 	if(message.content.startsWith(`${prefix}test`)) {
-		  message.channel.send('Test');
+		let embed = "dhuy";
+		message.channel.send({embed});
 	}
 })
 client.login(process.env.BOT_TOKEN);
