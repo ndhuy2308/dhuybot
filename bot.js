20dc89071c53ef7eda02de91775ce39b8f6db87a
ndhuy2308 <31300525+ndhuy2308@users.noreply.github.com>
2019-09-25 15:34:13 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -32,6 +32,7 @@ client.on('message', (message, member) => {
 		message.channel.send("BUỔI SÁNG:\nThứ 2: Văn - Văn - Toán - Toán\nThứ 3: Lý - Lý - Hóa - Sinh - Sinh\nThứ 4: Anh - Anh - Toán - Lý - GDCD\nThứ 5: Văn - Văn - Anh - Sử - Địa\nThứ 6: Hóa - Hóa - Toán - Toán - Sinh hoạt\n------------------------\nBUỔI CHIỀU:\nThứ 2 (Tiết 2345): Thể dục - Thể dục - Lý - Lý\nThứ 4 (Tiết 234): KTCN - KTCN - Quốc phòng \nThứ 6 (Tiết 2345): Toán - Toán - Tin - Tin")	
 	}
 	if(message.content.startsWith(`${prefix}test`)) {
+		var uid = message.author.id;
 		var embed = new Discord.RichEmbed()
 		.setColor('#0099ff')
   		.setAuthor("DHuy BOT - Thời khóa biểu", message.author.avatarURL)
@@ -45,7 +46,7 @@ client.on('message', (message, member) => {
 		.addField("Thứ 2 (Tiết 2345):","Thể dục - Thể dục - Lý - Lý")
 		.addField("Thứ 4 (Tiết 234):","KTCN - KTCN - Quốc phòng ")
 		.addField("Thứ 6 (Tiết 2345):","Toán - Toán - Tin - Tin")
-		.addField("Username",message.author.id)
+		.addField("Username",uid)
 		;
   		message.channel.send(embed);
 	}
