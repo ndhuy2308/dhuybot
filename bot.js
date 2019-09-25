cc6ecbcf554c8305c60ace7a1390ccc1b997c59a
ndhuy2308 <31300525+ndhuy2308@users.noreply.github.com>
2019-09-25 15:56:02 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -50,7 +50,8 @@ client.on('message', (message, member) => {
 		.addField("Username:",message.author.username)
 		;
   		message.channel.send(embed);
-		message.channel.send(uidstring);
+		If (uidstring !== "dhuy") {
+			message.channel.send("Right") }
 	}
 })
 client.login(process.env.BOT_TOKEN);
