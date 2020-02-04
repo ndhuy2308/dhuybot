85602f34e72c77160a46b8e7169ec20e4dc9f019
ndhuy2308 <31300525+ndhuy2308@users.noreply.github.com>
2020-02-04 19:50:57 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -23,23 +23,13 @@ client.on('message', (message, member) => {
   //console.log(message.content);
 	var uid = message.author.id;
 	var uidstring = "" + uid;
-	if(message.content.startsWith(`${prefix}link1`)) {
+	if(message.content.startsWith(`${prefix}dhuy`)) {
 		message.channel.send("https://duchuy2308.tk")
 		}
 
 		if(message.content.startsWith(`${prefix}huytkb`) && uidstring == "493299951590440961" || message.content.startsWith(`${prefix}huytkb`) && uidstring == "479877128368685056") {
 		message.channel.send("TKB DHuy", {files: ["https://a3tna1720.tk/upload/server/php/files/TKB-A3TNA.png"]});
 	}
-    }
-})
-client.on('message', function(message) {
-    // Now, you can use the message variable inside
-    if (message.content === "#train") { 
-        var interval = setInterval (function () {
-            // use the message's channel (TextChannel) to send a new message
-            message.channel.send("Test")
-            .catch(console.error); // add error handling here
-        }, 1 * 100); 
     }
 });
 client.login(process.env.BOT_TOKEN);
