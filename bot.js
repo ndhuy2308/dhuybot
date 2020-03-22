7622adb73a8ed366570a47397858ac85b02a427d
ndhuy2308 <31300525+ndhuy2308@users.noreply.github.com>
2020-03-22 22:22:49 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -68,6 +68,14 @@ client.on("message", (message) => {
         message.channel.send("Ban khong co quyen xem anh PKN.");
       }  	  	  
   }
+ if (message.content.startsWith(prefix + 'avatar')) {
+    const user = message.mentions.users.first() || message.author;
+    const avatarEmbed = new Discord.RichEmbed()
+        .setColor(0x333333)
+        .setAuthor(user.username)
+        .setImage(user.avatarURL);
+    message.channel.send(avatarEmbed);
+}
 });
 
 client.login(process.env.BOT_TOKEN);
